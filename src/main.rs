fe39dc13c7da4b812b8bdbb599b385e7d7d24e99
Mika <mika.marcondes2@gmail.com>
2024-02-24 16:27:55 -0300

    Spawn paddles

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,28 @@ use bevy::sprite::MaterialMesh2dBundle;
 
 const BALL_SIZE: f32 = 5.;
 
+const PADDLE_SPEED: f32 = 1.;
+const PADDLE_WIDTH: f32 = 10.;
+const PADDLE_HEIGHT: f32 = 50.;
+
+#[derive(Component)]
+struct Paddle;
+
+#[derive(Bundle)]
+struct PaddleBundle {
+    paddle: Paddle,
+    position: Position,
+}
+
+impl PaddleBundle {
+    fn new(x: f32, y: f32) -> Self {
+        PaddleBundle {
+            paddle: Paddle,
+            position: Position(Vec2::new(x, y)),
+        }
+    }
+}
+
 #[derive(Component)]
 struct Position(Vec2);
 
@@ -27,7 +49,7 @@ impl BallBundle {
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (spawn_ball, spawn_camera))
+        .add_systems(Startup, (spawn_ball, spawn_camera, spawn_paddles))
         .add_systems(Update, (move_ball, project_positions.after(move_ball)))
         .run();
 }
@@ -67,4 +89,27 @@ fn project_positions(mut positionable: Query<(&mut Transform, &Position)>) {
 }
 fn spawn_camera(mut commands: Commands) {
     commands.spawn_empty().insert(Camera2dBundle::default());
+}
+
+fn spawn_paddles(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    println!("Spawning paddles...");
+
+    let mesh = Mesh::from(Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT));
+    let material = ColorMaterial::from(Color::rgb(0., 1., 0.));
+
+    let mesh_handle = meshes.add(mesh);
+    let material_handle = materials.add(material);
+
+    commands.spawn((
+        PaddleBundle::new(20., -25f32),
+        MaterialMesh2dBundle {
+            mesh: mesh_handle.into(),
+            material: material_handle,
+            ..default()
+        },
+    ));
 }
