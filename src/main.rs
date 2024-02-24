ee3a717244ca8dafc90575ff8521b30f4a29ee85
Mika <mika.marcondes2@gmail.com>
2024-02-24 15:11:40 -0300

    BallBundle & positioning

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,48 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Position(Vec2);
+
+#[derive(Component)]
+struct Ball;
+
+#[derive(Bundle)]
+struct BallBundle {
+    ball: Ball,
+    position: Position,
+}
+
+impl BallBundle {
+    fn new() -> Self {
+        BallBundle {
+            ball: Ball,
+            position: Position(Vec2::new(0., 0.)),
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_ball, spawn_camera))
+        .add_systems(Update, project_positions)
         .run();
 }
 
 fn spawn_ball(mut commands: Commands) {
     println!("Spawning ball...");
-    commands.spawn_empty();
+
+    commands
+        .spawn_empty()
+        .insert(Transform::default())
+        .insert(BallBundle::new());
 }
 
+fn project_positions(mut positionable: Query<(&mut Transform, &Position)>) {
+    for (mut transform, position) in &mut positionable {
+        transform.translation = position.0.extend(0.);
+    }
+}
 fn spawn_camera(mut commands: Commands) {
     commands.spawn_empty().insert(Camera2dBundle::default());
 }
